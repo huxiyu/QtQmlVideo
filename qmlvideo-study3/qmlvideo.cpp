63d9407233ea97002723b96224aa331cc5a6f196
alex <Alex_cadcg@163.com>
2017-03-27 23:47:25 +0800

    draw triangle in technicolor!

--- qmlvideo-study3/qmlvideo.cpp
+++ qmlvideo-study3/qmlvideo.cpp
@@ -1,5 +1,6 @@
 #include "qmlvideo.h"
 #include <QPainter>
+#include <GL/glew.h>
 #include <qgl.h>
 #include <QDebug>
 
@@ -9,6 +10,19 @@ QmlVideo::QmlVideo(QQuickItem *parent)
     :QQuickPaintedItem(parent)
 {
     setRenderTarget(QQuickPaintedItem::FramebufferObject);
+    GLenum err = glewInit();
+        if (GLEW_OK != err)
+        {
+        }
+        else
+        {
+            if(GLEW_EXT_pixel_buffer_object)
+            {
+            }
+            else
+            {
+            }
+        }
 }
 
 QmlVideo::~QmlVideo(){
@@ -18,28 +32,25 @@ QmlVideo::~QmlVideo(){
 
 void QmlVideo::paint(QPainter* p)
 {
-    p->drawLine(QPoint(0,0), QPoint(10, 10));
-
     p->beginNativePainting();
 
     qDebug() << "Paint...";
-//    glClearColor(1, 1, 0, 1);
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
     QRectF rect = boundingRect();
-    qDebug() << rect.x() << " " << rect.y() << " " <<
-             rect.width() << " " << rect.height();
-
 
     glLoadIdentity();
-    glBegin(GL_QUADS);
-        glColor3f(0,0,0);
-        glVertex2d(rect.x(), rect.y());
-        glVertex2d(rect.x(), rect.y() + rect.height());
-        glVertex2d(rect.x() + rect.width(), rect.y() + rect.height());
-        glVertex2d(rect.x() + rect.width(), rect.y());
-    glEnd();
-
+        // draw triangle
+        glColor3f(1.0f,0.0f,0.0f);
+        glBegin(GL_TRIANGLES);
+                glColor3f(1.0f,0.0f,0.0f);
+                glVertex3f(0.0f, -1.0f, 0.0f);
+                glColor3f(0.0f,1.0f,0.0f);
+                glVertex3f(-1.0f, 1.0f, 0.0f);
+                glColor3f(0.0f,0.0f,1.0f);
+                glVertex3f(1.0f, 1.0f, 0.0f);
+        glEnd();
     p->endNativePainting();
 }
 
